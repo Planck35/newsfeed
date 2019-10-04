e86a067e6db525aecf83720365cac65578592f25
Planck35 <chaoyuw@andrew.cmu.edu>
2019-10-04 13:03:05 -0700

    Modify generation task.

---
+++ src/main/java/com/martinkl/samza/newsfeed/GenerateFollowsApplication.java
@@ -0,0 +1,38 @@
+package com.martinkl.samza.newsfeed;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.samza.application.TaskApplication;
+import org.apache.samza.application.descriptors.TaskApplicationDescriptor;
+import org.apache.samza.serializers.JsonSerde;
+import org.apache.samza.system.kafka.descriptors.KafkaOutputDescriptor;
+import org.apache.samza.system.kafka.descriptors.KafkaSystemDescriptor;
+import org.apache.samza.task.StreamTaskFactory;
+
+public class GeneraetFollowsApplication implements TaskApplication {
+    private static final List<String> KAFKA_CONSUMER_ZK_CONNECT = ImmutableList.of("localhost:2181");
+    private static final List<String> KAFKA_PRODUCER_BOOTSTRAP_SERVERS = ImmutableList.of("localhost:9092");
+    private static final Map<String, String> KAFKA_DEFAULT_STREAM_CONFIGS = ImmutableMap.of("replication.factor", "1");
+
+    @Override
+    public void describe(TaskApplicationDescriptor taskApplicationDescriptor) {
+        // Define a system descriptor for Kafka.
+        KafkaSystemDescriptor kafkaSystemDescriptor =
+                new KafkaSystemDescriptor("kafka").withConsumerZkConnect(KAFKA_CONSUMER_ZK_CONNECT)
+                        .withProducerBootstrapServers(KAFKA_PRODUCER_BOOTSTRAP_SERVERS)
+                        .withDefaultStreamConfigs(KAFKA_DEFAULT_STREAM_CONFIGS);
+
+        KafkaOutputDescriptor kafkaOutputDescriptor =
+                kafkaSystemDescriptor.getOutputDescriptor("newsfeed-follows", new JsonSerde<>());
+
+        taskApplicationDescriptor.withDefaultSystem(kafkaSystemDescriptor);
+
+        taskApplicationDescriptor.withOutputStream(kafkaOutputDescriptor);
+
+        taskApplicationDescriptor.withTaskFactory((StreamTaskFactory) () -> new GenerateFollowsTask());
+    }
+}
