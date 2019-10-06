010b631328763a00bc325d126706083b1b807a42
Planck35 <chaoyuw@andrew.cmu.edu>
2019-10-06 21:12:02 +0000

    change the launch script.

--- launch.sh
+++ launch.sh
@@ -16,6 +16,6 @@ tar -xvf target/newsfeed-0.0.1-dist.tar.gz -C deploy/samza/
 # Copy the tar.gz file to hadoop file system
 hadoop fs -copyFromLocal -f target/newsfeed-0.0.1-dist.tar.gz /
 
+deploy/samza/bin/run-app.sh --config-factory=org.apache.samza.config.factories.PropertiesConfigFactory --config-path="file://$PWD/deploy/samza/config/newsfeed-fan-out.properties"
 deploy/samza/bin/run-app.sh --config-factory=org.apache.samza.config.factories.PropertiesConfigFactory --config-path="file://$PWD/deploy/samza/config/newsfeed-generate-follows.properties"
 deploy/samza/bin/run-app.sh --config-factory=org.apache.samza.config.factories.PropertiesConfigFactory --config-path="file://$PWD/deploy/samza/config/newsfeed-generate-messages.properties"
-deploy/samza/bin/run-app.sh --config-factory=org.apache.samza.config.factories.PropertiesConfigFactory --config-path="file://$PWD/deploy/samza/config/newsfeed-fan-out.properties"
